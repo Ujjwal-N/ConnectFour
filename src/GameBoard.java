867aa55bd69976267976f617fcae0ceea3f99d56
Ujjwal-N <ujjwal.nadhani@gmail.com>
2018-04-17 11:28:59 -0700

    possibly working opponentPieceArray

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,7 +1,8 @@
-
+import java.util.ArrayList;
 
 public class GameBoard {
 	Piece[][] array;
+	ArrayList<Piece> opponentPieces = new ArrayList<Piece>();
 	public GameBoard() {
 		array = new Piece[7][6];
 		for(int columnCount = 0; columnCount < array.length; columnCount++) {
@@ -9,6 +10,11 @@ public class GameBoard {
 				array[columnCount][rowCount] = new Piece(Piece.Type.EMPTY, columnCount, rowCount);
 			}
 		}
+	}
+	public void updateOpponentPieceArray(Piece opponentPiece){
+		opponentPieces.add(opponentPiece.clone());
+		System.out.println(opponentPieces);
+		//((Appendable) opponentPieces).append(opponentPiece.clone());
 	}
 	public void playMove(Piece piece) {
 		array[piece.getColumn()][piece.getRow()] = piece.clone();
@@ -28,6 +34,9 @@ public class GameBoard {
 			}
 		}
 		return 5;
+	}
+	public int threatDetect(){
+		return -1;
 	}
 	public Piece getPieceDiff(Piece piece, int xDiff, int yDiff) {
 		return array[piece.getColumn() + yDiff][piece.getRow() + xDiff];
